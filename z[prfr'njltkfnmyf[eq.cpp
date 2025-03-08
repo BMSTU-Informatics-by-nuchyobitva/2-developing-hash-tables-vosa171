f0e337d5277d88418f74382615176f5e234b3c63
vosa171 <vosa220@gmail.com>
2025-03-09 00:15:41 +0300

    z[prfr'njltkfnmyf[eq.cpp

--- z[prfr'njltkfnmyf[eq.cpp
+++ z[prfr'njltkfnmyf[eq.cpp
@@ -11,13 +11,13 @@ public:
     std::string wo;
 };
 
-void print(const std::vector<std::string>& wo) {
+void print(const std::vector<std::string>& wo) {//понятно из названия
     for (int i = 0; i < wo.size(); i++) {
         std::cout << i << " " << wo[i] << std::endl;
     }
     std::cout << std::endl;
 }
-void init(std::vector<std::string>& wo, int n1, int n2, const std::string& st) {
+void init(std::vector<std::string>& wo, int n1, int n2, const std::string& st) {//отвечает за изменение
     
     if (wo[n1] == "nullptr") {
         wo[n1] = st;
@@ -25,23 +25,23 @@ void init(std::vector<std::string>& wo, int n1, int n2, const std::string& st) {
     else {
         int y = n1; 
         while (y < wo.size() && wo[y] != "nullptr") {
-            y += n2; 
+            y += n2; //шаг
         }
         if (y < wo.size()) {
             wo[y] = st; 
         }
     }
 }
-void remove(std::vector<std::string>& wo, int n1) {
+void remove(std::vector<std::string>& wo, int n1) {//убирает элемент
     wo[n1] = "nullptr";
     
 }
-void clear(std::vector<std::string>& wo) {
+void clear(std::vector<std::string>& wo) {//убирает всё
     for (int i = 0; i < wo.size(); i++) {
         wo[i] = "nullptr";
     }
 }
-void find(const std::vector<std::string>& wo, const std::string& st) {
+void find(const std::vector<std::string>& wo, const std::string& st) {//ищет через схожесть
     for (int i = 0; i < wo.size(); i++) {
         if (wo[i] == st) {
             std::cout << i << " " << wo[i] << std::endl;
@@ -50,9 +50,9 @@ void find(const std::vector<std::string>& wo, const std::string& st) {
 }
 int main() {
     int n;
-    std::cin >> n;
-    std::vector<std::string> wo(n, "nullptr");
-
+    std::cin >> n;//задаём размер
+    std::vector<std::string> wo(n, "nullptr");//инициализируем
+//далее идут сами действия
     init(wo, 2, 1, "try hard");
     init(wo, 2, 3, "try hard");
     find(wo, "try hard");
