1099112953dca8c5db69624812805ce56e84c7df
vosa171 <vosa220@gmail.com>
2025-03-08 23:58:06 +0300

    Add files via upload

---
+++ z[prfr'njltkfnmyf[eq.cpp
@@ -0,0 +1,64 @@
+﻿//a%4+3
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+
+class words {
+public:
+    int n1;
+    int n2;
+    std::string wo;
+};
+
+void print(const std::vector<std::string>& wo) {
+    for (int i = 0; i < wo.size(); i++) {
+        std::cout << i << " " << wo[i] << std::endl;
+    }
+    std::cout << std::endl;
+}
+void init(std::vector<std::string>& wo, int n1, int n2, const std::string& st) {
+    
+    if (wo[n1] == "nullptr") {
+        wo[n1] = st;
+    }
+    else {
+        int y = n1; 
+        while (y < wo.size() && wo[y] != "nullptr") {
+            y += n2; 
+        }
+        if (y < wo.size()) {
+            wo[y] = st; 
+        }
+    }
+}
+void remove(std::vector<std::string>& wo, int n1) {
+    wo[n1] = "nullptr";
+    
+}
+void clear(std::vector<std::string>& wo) {
+    for (int i = 0; i < wo.size(); i++) {
+        wo[i] = "nullptr";
+    }
+}
+void find(const std::vector<std::string>& wo, const std::string& st) {
+    for (int i = 0; i < wo.size(); i++) {
+        if (wo[i] == st) {
+            std::cout << i << " " << wo[i] << std::endl;
+        }
+    }
+}
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<std::string> wo(n, "nullptr");
+
+    init(wo, 2, 1, "try hard");
+    init(wo, 2, 3, "try hard");
+    find(wo, "try hard");
+    print(wo);
+    clear(wo);
+    print(wo);
+    return 0;
+}
+//лучше не будет, потому что не умею иначе
